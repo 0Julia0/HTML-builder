201e27aeed6c660431b0497818a74adf105a4653
Julia Pinkevich <yuliapinkevitch@yandex.by>
2022-05-25 21:00:52 +0300

    fix: fix task 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -64,21 +64,6 @@ fs.access(path.join(__dirname, 'project-dist'), function(err) {
                             if (err) throw err;
                           }
                         );
-
-                        fs.readFile((path.join(__dirname, 'components', 'about.html')), (err, data) => {
-                          if (err) {
-                            console.log(err);
-                          } else {
-                            const tamplateWithAbout = tamplateWithFooter.replace(/{{about}}/g, data);
-                            fs.writeFile(
-                              path.join(__dirname, 'project-dist', 'index.html'),
-                              tamplateWithAbout,
-                              (err) => {
-                                if (err) throw err;
-                              }
-                            );
-                          }
-                        });
                       }
                     });
                   }
@@ -187,6 +172,150 @@ fs.access(path.join(__dirname, 'project-dist'), function(err) {
       }
     });
   } else {
+    fs.readdir(path.join(__dirname, 'project-dist', 'assets', 'fonts'), {withFileTypes: true}, (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          fsPromises.unlink((path.join(__dirname, 'project-dist', 'assets', 'fonts', file.name)))
+            .then( function () {
+            })
+            . catch ( function (error) {
+              console.log(error);
+            });
+        });
+      }
+    });
+
+    fs.readdir(path.join(__dirname, 'project-dist', 'assets', 'img'), {withFileTypes: true}, (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          fsPromises.unlink((path.join(__dirname, 'project-dist', 'assets', 'img', file.name)))
+            .then( function () {
+            })
+            . catch ( function (error) {
+              console.log(error);
+            });
+        });
+      }
+    });
+
+    fs.readdir(path.join(__dirname, 'project-dist', 'assets', 'svg'), {withFileTypes: true}, (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          fsPromises.unlink((path.join(__dirname, 'project-dist', 'assets', 'svg', file.name)))
+            .then( function () {
+            })
+            . catch ( function (error) {
+              console.log(error);
+            });
+        });
+      }
+    });
+
+    fs.readdir(path.join(__dirname, 'project-dist'), {withFileTypes: true}, (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          fsPromises.unlink((path.join(__dirname, 'project-dist', file.name)))
+            .then( function () {
+            })
+            . catch ( function (error) {
+              console.log(error);
+            });
+        });
+      }
+    });
+
+    fs.readFile((path.join(__dirname, 'template.html')), (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const template = data.toString();
+  
+        fs.writeFile(
+          path.join(__dirname, 'project-dist', 'index.html'),
+          template,
+          (err) => {
+            if (err) throw err;
+          }
+        );
+  
+        fs.readFile((path.join(__dirname, 'project-dist', 'index.html')), (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const newTemplate = data.toString();
+  
+            fs.readFile((path.join(__dirname, 'components', 'header.html')), (err, data) => {
+              if (err) {
+                console.log(err);
+              } else {
+                const tamplateWithHeader = newTemplate.replace(/{{header}}/g, data);
+                fs.writeFile(
+                  path.join(__dirname, 'project-dist', 'index.html'),
+                  tamplateWithHeader,
+                  (err) => {
+                    if (err) throw err;
+                  }
+                );
+  
+                fs.readFile((path.join(__dirname, 'components', 'articles.html')), (err, data) => {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    const tamplateWithArticles =  tamplateWithHeader.replace(/{{articles}}/g, data);
+                    fs.writeFile(
+                      path.join(__dirname, 'project-dist', 'index.html'),
+                      tamplateWithArticles,
+                      (err) => {
+                        if (err) throw err;
+                      }
+                    );
+  
+                    fs.readFile((path.join(__dirname, 'components', 'footer.html')), (err, data) => {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        const tamplateWithFooter = tamplateWithArticles.replace(/{{footer}}/g, data);
+                        fs.writeFile(
+                          path.join(__dirname, 'project-dist', 'index.html'),
+                          tamplateWithFooter,
+                          (err) => {
+                            if (err) throw err;
+                          }
+                        );
+
+                        fs.readFile((path.join(__dirname, 'components', 'about.html')), (err, data) => {
+                          if (err) {
+                            console.log(err);
+                          } else {
+                            const tamplateWithAbout = tamplateWithFooter.replace(/{{about}}/g, data);
+                            fs.writeFile(
+                              path.join(__dirname, 'project-dist', 'index.html'),
+                              tamplateWithAbout,
+                              (err) => {
+                                if (err) throw err;
+                              }
+                            );
+                          }
+                        });
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+
     fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}, (err, files) => {
       if (err)
         console.log(err);
